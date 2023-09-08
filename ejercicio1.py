0d47d8ad3a8fa23326e4d85f0f5b8cc3869079aa
AJmarroquin25100 <136849629+AJmarroquin25100@users.noreply.github.com>
2023-09-08 16:09:42 -0600

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+nombre = input("Coloque su nombre: ")
+entero = int(input("Coloque un numero entero: "))
+
+
+for i in range(entero):
+    print(nombre)
---
+++ ejercicio5.py
@@ -0,0 +1,10 @@
+frase = input("Ingresa una frase: ")
+
+#print(frase[::-1]) primera opcion
+
+
+count = len(frase) -1
+
+for i in range(0, count +1):
+    print(frase[count], end=" ")
+    count-=1
